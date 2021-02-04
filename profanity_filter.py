be31e4f6be10ca5aceb56fb8a0061257b3b49114
jbell1991 <joseph.louis.bell@gmail.com>
2021-02-04 16:59:41 -0500

    filtered profanity out

---
+++ profanity_filter.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# load in bad words
+df = pd.read_csv('bad_single.csv', usecols=[0], names=None)
+
+# convert to list
+bad_words = df['Bad_words'].to_list()
+
+
+def return_bad_words(text):
+    """Takes in plain text without punctuation, separated by spaces
+    and returns text with profanity removed"""
+    text = text.split()
+    for word in bad_words:
+        # if word in text:
+        #     flagged_list.append(word)
+        while word in text:
+            # remove the word from text
+            text.remove(word)
+    return " ".join(text)
